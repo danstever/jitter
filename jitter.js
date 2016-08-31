624e72a6e63de52b208006cd69137fdeebf22c3d
Dan Stever <danstever@gmail.com>
2016-08-31 08:58:27 -0400

    clean up

--- jitter.js
+++ jitter.js
@@ -1,16 +1,14 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function() {
+chrome.webNavigation.onBeforeNavigate.addListener(function(){
     chrome.windows.getCurrent(function(win){
         var wQty = Math.round(Math.random()*5) + 1;
-        wQty *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+        wQty *= Math.round(Math.random()) == 1 ? 1 : -1;
 
         var hQty = Math.round(Math.random()*5) + 1;
-        hQty *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+        hQty *= Math.round(Math.random()) == 1 ? 1 : -1;
 
         var width = win.width + wQty;
         var height = win.height + hQty;
 
-        chrome.windows.update(win.id, {'width': width, 'height': height }, function(){
-
-        });
+        chrome.windows.update(win.id, {'width': width, 'height': height }, function(){});
     });
 });
